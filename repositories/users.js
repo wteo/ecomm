9a1a81720cc609e6246177b0862a23ff3f0d09ae
wteo <wendyteo.wy@gmail.com>
2022-04-11 08:28:27 +1000

    included authentication process

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const crypto = require("crypto");
+const util = require("util");
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
     constructor(filename) {
@@ -23,12 +26,29 @@ class UsersRepository {
 
     async create(attrs) {
         attrs.id = this.randomId();
+        
+        const salt = crypto.randomBytes(8).toString("hex");
+        const buf = await scrypt(attrs.password, salt, 64);
+        
         const records = await this.getAll();
-        records.push(attrs);
+        const record = {
+            ...attrs,
+            password: `${buf.toString("hex")}.${salt}`
+        }
+        records.push(record);
 
         await this.writeAll(records);
 
-        return attrs;
+        return record;
+    }
+
+    async comparePasswords(saved, supplied) {
+        // Saved --> password saved in our database. "hashed.salt"
+        // Supplied --> password given to us by a user trying to signin
+        const [hashed, salt] = saved.split(".");
+        const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+        return hashed === hashedSuppliedBuf.toString("hex");
     }
 
     async writeAll(records) {
