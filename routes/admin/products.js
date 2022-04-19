7a6325d5d59371e573a0ab509eff3531c2531606
wteo <wendyteo.wy@gmail.com>
2022-04-19 11:42:07 +1000

    Added validation to the form

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { validationResult } = require("express-validator");
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
+const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
 
@@ -11,5 +13,16 @@ router.get("/admin/products", (req, res) => {
 router.get("/admin/products/new", (req, res) => {
     res.send(productsNewTemplate({}));
 });
+
+router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    res.send("Sumbitted");
+});
+
+router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
+    console.log();
+    res.send("Submitted.");
+})
 
 module.exports = router;
