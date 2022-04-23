8a7b4e66384168e4fe020265edc22397a122e659
wteo <wendyteo.wy@gmail.com>
2022-04-23 10:16:48 +1000

    Listing various products on screen

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
-const productsRouter = require("./routes/admin/products");
+const adminProductsRouter = require("./routes/admin/products");
+const productsRouter = require("./routes/products");
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cookieSession({
 
 app.use(authRouter);
 app.use(productsRouter);
+app.use(adminProductsRouter);
 
 app.listen(3000, () => {
     console.log("Listening");
